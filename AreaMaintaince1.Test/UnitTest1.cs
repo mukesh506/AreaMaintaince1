17e719e486d06b2c1eaba9b1e4c4cf5912656c91
MUKESH KUMAR JHA <kumarjhamukesh506@gmail.com>
2021-04-17 16:43:34 +0530

    Test Case add

---
+++ AreaMaintaince1.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace AreaMaintaince1.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("Test", "Test");
+        }
+    }
+}
